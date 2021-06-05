86043075d2ed6241b6d368892614cb2c2f0fe92c
ooreally <anu1512sri@gmail.com>
2021-06-05 20:36:29 +0530

    ADD: code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Account from './components/Settings/Account';
 import Coach from './components/Settings/Coach';
 import Header from './components/Header';
 import Learn from './components/Learn/Learn';
-import H2 from './components/H2';
+
 import './App.css';
 
 function App() {
